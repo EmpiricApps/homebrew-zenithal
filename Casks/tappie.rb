c01e72a8981a1ec09178d1129fdabd7b8f238614
Mihai Burlac <mihaiburlac@gmail.com>
2026-08-18 14:58:57 +0300

    tappie 1.6.0

--- Casks/tappie.rb
+++ Casks/tappie.rb
@@ -1,6 +1,6 @@
 cask "tappie" do
-  version "1.5.0"
-  sha256 "500d09d80e769347b116606badbc964ce621538c00c99152d7d3a8820a26d554"
+  version "1.6.0"
+  sha256 "3a7b82226ac12359b0f782a54937d95a9610ab44671776f00b8bfb98f3020d64"
 
   url "https://www.empiricapps.com/api/download/tappie?version=#{version}"
   name "Tappie"
